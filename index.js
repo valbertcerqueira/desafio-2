da9090ee8bd703c85a4c7aa1e781f4f2af8d3fef
valbertcerqueira <valbertcerqueira@gmail.com>
2023-11-11 12:18:33 -0300

    Atualizando código

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 
-let nivel = ""
+let level = ""
 
-function testeNivel(nome, xp) {
+function testLevel(name, xp) {
     if (xp <= 1000){
-        nivel = "Ferro"
+        level = "Ferro"
     }
     else if (xp >= 1001 && xp <= 2000){
-        nivel = "Bronze"
+        level = "Bronze"
     }
     else if (xp >= 2001 && xp <= 5000){
-        nivel = "Prata"
+        level = "Prata"
     }
     else if (xp >= 5001 && xp <= 7000){
-        nivel = "Ouro"
+        level = "Ouro"
     }
     else if (xp >= 7001 && xp <= 8000){
-        nivel = "Platina"
+        level = "Platina"
     }
     else if (xp >= 8001 && xp <= 9000){
-        nivel = "Ascendente"
+        level = "Ascendente"
     }
     else if (xp >= 9001 && xp <= 10000){
-        nivel = "Imortal"
+        level = "Imortal"
     }
     else if (xp >= 10001){
-        nivel = "Radiante"
+        level = "Radiante"
     }
-    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+    console.log(`O Herói de nome ${name} está no nível de ${level}`)
 }
-testeNivel("Goku", 11000)
+testLevel("Goku", 11000)
 
